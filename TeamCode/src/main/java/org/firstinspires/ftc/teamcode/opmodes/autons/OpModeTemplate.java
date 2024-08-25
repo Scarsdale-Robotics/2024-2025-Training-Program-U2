d7923aed4b9bb830b562e1a7f5fc52c4a703b88b
unknown <KK201431873@gmail.com>
2024-08-25 16:54:51 -0700

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autons/OpModeTemplate.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes.autons;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.HardwareRobot;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+
+public class OpModeTemplate extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        HardwareRobot robot = new HardwareRobot(hardwareMap);
+        DriveSubsystem drive = new DriveSubsystem(
+                robot.leftFront,
+                robot.rightFront,
+                robot.leftBack,
+                robot.rightBack
+        );
+
+        waitForStart();
+
+
+        // Write your code here
+
+
+    }
+}
